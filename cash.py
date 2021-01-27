82e9710c9ed00ef91690ddcce6a4d221af2e6b95
mgxmajewski <mgxmajewski@gmail.com>
2021-01-27 21:14:48 +0000

    Add conditions to count coins required to give change

--- cash.py
+++ cash.py
@@ -3,7 +3,32 @@ from cs50 import get_float
 
 # Prompts user to give valid number
 while True:
-    n = get_float("Change owed: ")
-    if n > 0:
+    dollar = get_float("Change owed: ")
+    if dollar > 0:
         break
     
+cents = round(dollar*100)
+coins = 0
+
+while cents >= 25:
+    cents -= 25    
+    coins += 1
+
+while cents >= 10:
+    cents -= 10    
+    coins += 1
+
+while cents >= 5:
+    cents -= 5    
+    coins += 1
+
+while cents >= 1:
+    cents -= 1    
+    coins += 1
+    
+print(coins)
+
+    
+
+
+
